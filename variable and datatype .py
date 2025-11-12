7cd090b2ba997085a51ff747005b21420189917f
venkatesh j m <125080129+venkisamarth@users.noreply.github.com>
2025-11-12 23:36:08 +0530

    Add files via upload

---
+++ variable and datatype .py
@@ -0,0 +1,20 @@
+print("\u2764")   # ❤
+print("\U0001F602")  # 😂
+
+x = 10 
+name = "Venkat"
+
+print(x)
+print(name)
+
+# multiple Varaible Assignments 
+x = y = z = 100
+print(x, y, z)
+
+a, b, c = 10, 20, 30
+print(a, b, c)
+
+x = 10       # int
+x = "Python" # now it's a str
+print(x)
+
